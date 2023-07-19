ec39ac021508bdf596ef6a2f3b896bc8c1d44d65
Valentyn <wolf.valentyn@gmail.com>
2023-07-19 18:42:02 +0300

    generate filter buttons dynamically from heroes.json

--- src/actions/index.js
+++ src/actions/index.js
@@ -17,6 +17,26 @@ export const heroesFetchingError = () => {
     }
 }
 
+export const filtersFetching = () => {
+    return {
+        type: 'FILTERS_FETCHING'
+    }
+}
+
+export const filtersFetched = (filters) => {
+    return {
+        type: 'FILTERS_FETCHED',
+        payload: filters
+    }
+}
+
+export const filtersFetchingError = () => {
+    return {
+        type: 'FILTERS_FETCHING_ERROR'
+    }
+}
+
+
 export const deleteHero = (id) => {
     return {
         type: 'DELETE_HERO',
--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -1,4 +1,3 @@
-
 // Задача для этого компонента:
 // Фильтры должны формироваться на основании загруженных данных
 // Фильтры должны отображать только нужных героев при выборе
@@ -6,17 +5,53 @@
 // Изменять json-файл для удобства МОЖНО!
 // Представьте, что вы попросили бэкенд-разработчика об этом
 
+import { useSelector, useDispatch } from "react-redux";
+import { useHttp } from "../../hooks/http.hook";
+import { filtersFetching, filtersFetched, filtersFetchingError } from "../../actions";
+import { useEffect } from "react";
+import Spinner from '../spinner/Spinner'
+
 const HeroesFilters = () => {
+    const dispatch = useDispatch();
+    const {filters, filtersLoadingStatus} = useSelector(state => state);
+    const {request} = useHttp();
+
+    useEffect(() => {
+        dispatch(filtersFetching());
+
+        request("http://localhost:3001/filters")
+            .then(data => dispatch(filtersFetched(data)))
+            .catch(() => dispatch(filtersFetchingError()));
+    }, [])
+
+    if (filtersLoadingStatus === "loading") {
+        return <Spinner/>;
+    } else if (filtersLoadingStatus === "error") {
+        return <h5 className="text-center mt-5">Loading error</h5>
+    }
+
+    const renderFilters = (arr) => {
+        if (arr.length === 0) {
+            return <h5 className="text-center mt-5">Filters are not found</h5>
+        }
+
+        return arr.map(({name, className, label}) => {
+            return <button
+                        key={name} 
+                        id={name}
+                        className={`btn ${className}`}
+                        >{label}</button>
+        })
+    }
+
+    const elements = renderFilters(filters);
+
     return (
         <div className="card shadow-lg mt-4">
             <div className="card-body">
                 <p className="card-text">Filter heroes by elemental powers</p>
                 <div className="btn-group">
-                    <button className="btn btn-outline-dark active">All</button>
-                    <button className="btn btn-danger">Fire</button>
-                    <button className="btn btn-primary">Water</button>
-                    <button className="btn btn-success">Wind</button>
-                    <button className="btn btn-secondary">Earth</button>
+                    {elements}
                 </div>
             </div>
         </div>
