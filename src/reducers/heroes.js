da483397a79e55118590e4b055922f189cc129e3
Valentyn <wolf.valentyn@gmail.com>
2023-07-27 16:02:51 +0300

    refactor reducers using Redux Toolkit's createReducer

--- src/reducers/heroes.js
+++ src/reducers/heroes.js
@@ -1,38 +1,36 @@
+import { createReducer } from "@reduxjs/toolkit"
+import {
+    heroesFetching,
+    heroesFetched,
+    heroesFetchingError,
+    deleteHero,
+    addHero
+} from '../actions';
+
 const initialState = {
     heroes: [],
     heroesLoadingStatus: 'idle'
 }
 
-const heroes = (state = initialState, action) => {
-    switch (action.type) {
-        case 'HEROES_FETCHING':
-            return {
-                ...state,
-                heroesLoadingStatus: 'loading'
-            }
-        case 'HEROES_FETCHED':
-            return {
-                ...state,
-                heroes: action.payload,
-                heroesLoadingStatus: 'idle'
-            }
-        case 'HEROES_FETCHING_ERROR':
-            return {
-                ...state,
-                heroesLoadingStatus: 'error'
-            }
-        case 'DELETE_HERO': 
-            return {
-                ...state,
-                heroes: state.heroes.filter(hero => hero.id !== action.payload)
-            }
-        case 'ADD_HERO':
-            return {
-                ...state,
-                heroes: [...state.heroes, action.payload]
-            }
-        default: return state
-    }
-}
+const heroes = createReducer(initialState, builder => {
+    builder
+        .addCase(heroesFetching, state => {
+            state.heroesLoadingStatus = 'loading';
+        })
+        .addCase(heroesFetched, (state, action) => {
+            state.heroesLoadingStatus = 'idle';
+            state.heroes = action.payload;
+        })
+        .addCase(heroesFetchingError, state => {
+            state.heroesLoadingStatus = 'error';
+        })
+        .addCase(deleteHero, (state, action) => {
+            state.heroes = state.heroes.filter(hero => hero.id !== action.payload);
+        })
+        .addCase(addHero, (state, action) => {
+            state.heroes.push(action.payload); 
+        })
+        .addDefaultCase(() => {});
+}) 
 
 export default heroes;
